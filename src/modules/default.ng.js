a0dbc6e79e7fdf2c001247d0c918c8995d442f32
JohnnyBeProgramming <Johnathan.Lodder@Gmail.com>
2015-01-29 17:14:29 +0200

    Updated blank template

---
+++ src/modules/default.ng.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('myApp.default', [
+    'views/default.tpl.html', // Requires template
+    'ngRoute',
+])
+
+    .config(['$stateProvider', function ($stateProvider) {
+        // Now set up the states
+        $stateProvider
+          .state('home', {
+              url: '/',
+              views: {
+                  'main@': {
+                      templateUrl: 'views/default.tpl.html',
+                      controller: 'HomeViewCtrl'
+                  },
+              }
+          })
+    }])
+
+    .controller('HomeViewCtrl', ['$scope', '$window', '$location', '$timeout', function ($scope, $window, $location, $timeout) {
+        // ToDo: Add code for home controller         
+    }]);
