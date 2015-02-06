f5a0fc0a627a32dc8e26dd87093fe1d39531a50e
JohnnyBeProgramming <Johnathan.Lodder@Gmail.com>
2015-02-06 19:52:58 +0200

    Adding SQLCMD module

--- src/modules/appCmd/module.ng.js
+++ src/modules/appCmd/module.ng.js
@@ -9,7 +9,7 @@ angular.module('myApp.appCmd', [
         // Now set up the states
         $stateProvider
           .state('appCmd', {
-              url: '/appCmd',
+              url: '/cmd',
               abstract: true,
           })
         .state('appCmd.discover', {
@@ -24,13 +24,13 @@ angular.module('myApp.appCmd', [
             }
         })
         .state('appCmd.connect', {
-            url: '/connect',
+            url: '/connect/:path/:file',
             views: {
                 'menu@': { templateUrl: 'modules/appCmd/views/menu.html' },
                 'left@': { templateUrl: 'modules/appCmd/views/left.html' },
                 'main@': {
-                    templateUrl: 'modules/appCmd/views/index.tpl.html',
-                    controller: 'appCmdViewController'
+                    templateUrl: 'modules/appCmd/views/connect.tpl.html',
+                    controller: 'sqlCmdViewController'
                 },
             }
         })
@@ -41,13 +41,13 @@ angular.module('myApp.appCmd', [
                 'left@': { templateUrl: 'modules/appCmd/views/left.html' },
                 'main@': {
                     templateUrl: 'modules/appCmd/views/index.tpl.html',
-                    controller: 'appCmdViewController'
+                    controller: 'sqlCmdViewController'
                 },
             }
         })
     }])
 
-    .controller('appCmdViewController', ['$scope', '$rootScope', '$window', '$location', '$timeout', function ($scope, $rootScope, $window, $location, $timeout) {
+    .controller('appCmdViewController', ['$rootScope', '$scope', '$state', '$window', '$location', '$timeout', function ($rootScope, $scope, $state, $window, $location, $timeout) {
         // Define the appCmd model
         $scope.appCmd = {
             busy: true,
@@ -72,9 +72,15 @@ angular.module('myApp.appCmd', [
 
                     return css;
                 },
+                call: function (path, file) {
+                    if (/(sqlcmd\.exe)/i.test(file)) {
+                        var params = { path: path, file: file };
+                        $state.transitionTo('appCmd.connect', params);
+                    }
+                },
                 list: function (path, callback) {
                     try {
-                        console.info(' - [ appCmd ] List path: ' + path);
+                        console.info(' - [ appCmd ] List: ' + path);
 
                         var list = [];
                         var regex = /(\d{2}\/\d{2}\/\d{4})  (\d{2}:\d{2} \w{2})([ ]+\d+,\d+ )(\w+.\w+)/;
@@ -107,9 +113,6 @@ angular.module('myApp.appCmd', [
                                                 },
                                             });
                                         }
-                                    });
-                                    $timeout(function () {
-                                        
                                     });
                                 });
                             });
@@ -195,5 +198,196 @@ angular.module('myApp.appCmd', [
             updates.error = ex;
         }
         angular.extend($scope.appCmd, updates);
+
+    }])
+    .controller('sqlCmdViewController', ['$rootScope', '$scope', '$state', '$stateParams', function ($rootScope, $scope, $state, $stateParams) {
+
+        // Define the appCmd model
+        $scope.sqlCmd = {
+            busy: true,
+            exec: $stateParams.file,
+            path: $stateParams.path,
+            utils: {
+                icon: function (path, file) {
+                    var css = '';
+                    if (file) {
+                        css = 'glyphicon-file';
+                        if (/(.*)?.sql$/i.test(file)) css = 'glyphicon-cog'
+                    } else {
+                        var target = $scope.appCmd.target;
+                        if (target && (path == target.path)) {
+                            css += 'glyphicon-folder-open glow-blue';
+                        } else {
+                            css += 'glyphicon-folder-open glow-orange';
+                        }
+                    }
+
+                    return css;
+                },
+                call: function (path, file) {
+                    if (/(.*?\.sql)/i.test(file)) {
+                        //$state.go('sqlCmd.edit', {});
+                    }
+                },
+                list: function (host, callback, errorHandler) {
+                    try {
+                        console.info(' - [ sqlCmd ] Connect: ' + host);
+
+                        var list = [];
+                        var regex = /(\d{2}\/\d{2}\/\d{4})  (\d{2}:\d{2} \w{2})([ ]+\d+,\d+ )(\w+.\w+)/;
+                        var proc = require("child_process");
+                        if (proc) {
+                            var commands = [];
+
+                            //commands.push('dir "' + path + '\\"');
+                            var extensions = ['.exe', '.cmd', '.cer', '.pem', '.htm*'];
+                            extensions.forEach(function (ext) {
+                                commands.push('dir "' + path + '\\*' + ext + '"');
+                            });
+
+                            commands.forEach(function (cmd) {
+                                proc.exec(cmd, function (error, stdout, stderr) {
+                                    stdout.split('\n').forEach(function (line) {
+                                        var result = regex.exec(line);
+                                        if (result && result.length > 4) {
+                                            list.push(result[4]);
+                                        }
+                                    });
+                                    $rootScope.$applyAsync(function () {
+                                        if (callback) {
+                                            callback(list);
+                                        } else {
+                                            angular.extend($scope.appCmd, {
+                                                target: {
+                                                    path: path,
+                                                    list: list,
+                                                },
+                                            });
+                                        }
+                                    });
+                                });
+                            });
+                        }
+                    } catch (ex) {
+                        console.error(ex.message);
+                        $scope.appCmd.error = ex;
+                    }
+                },
+                exec: function (sql, callback, errorHandler) {
+
+                    function runSql(tmp) {
+                        var ext = ' -i "' + path.join(process.cwd(), tmp) + '"';
+                        var arg = ' -S lpc:localhost -E';
+                        var cmd = $scope.sqlCmd.exec + arg + ext;
+                        var opts = {
+                            cwd: $scope.sqlCmd.path,
+                        };
+                        updates.opts = opts;
+
+                        // Parse the system paths
+                        var proc = require("child_process");
+                        if (proc) {
+                            console.info(' - [ sqlCmd ] Calling: ' + cmd);
+                            proc.exec(cmd, opts, function (error, stdout, stderr) {
+                                $rootScope.$applyAsync(function () {
+                                    updates.active = !error;
+                                    updates.busy = false;
+                                    if (error) {
+                                        updates.error = {
+                                            message: 'Command Failed: ' + error.cmd,
+                                            context: error,
+                                        };
+                                        if (errorHandler) {
+                                            errorHandler(updates.error);
+                                        }
+                                    } else {
+                                        // Parse the result
+                                        var result = stdout;
+                                        if (callback) {
+                                            callback(result);
+                                        }
+                                    }
+                                    angular.extend($scope.sqlCmd, {
+                                        result: {
+                                            stdout: stdout,
+                                            stderr: stderr,
+                                        },
+                                    });
+                                });
+                            });
+                        }
+                    }
+
+                    var fs = require('fs');
+                    var tmp = './tmp/sql/dynamic.' + Date.now() + '.sql';
+                    fs.exists(tmp, function (exists) {
+                        if (!exists) {
+                            console.debug(' - Writing file: ' + tmp);
+                            fs.writeFile(tmp, sql, function (err) {
+                                if (err) {
+                                    if (errorHandler) {
+                                        errorHandler(err);
+                                    } else {
+                                        console.error(err);
+                                    }
+                                } else {
+                                    console.log(" - The file was saved!");
+                                    runSql(tmp);
+                                }
+                            });
+                        } else {
+                            console.debug(' - File exists: ' + tmp);
+                            runSql(tmp);
+                        }
+                    });
+                },
+            },
+        };
+
+        var updates = {};
+        try {
+            // Check for required libraries
+            if (typeof require !== 'undefined') {
+                var path = require('path');
+
+                // Set the result
+                updates = {
+                    busy: false,
+                    active: false,
+                    result: {}
+                };
+
+
+                // Get the list of database currently available
+                $scope.sqlCmd.utils.exec('EXEC sp_databases', function (result) {
+                    console.debug(result);
+                });
+
+                // Get the current working folder
+                var cwd = (typeof process !== 'undefined') ? process.cwd() : null;
+                if (cwd) {
+                    // List current folder contents
+                    /*
+                    $scope.sqlCmd.utils.list(cwd, function (list) {
+                        $rootScope.$applyAsync(function () {
+                            // Update the current working dir
+                            $scope.appCmd.cwd = {
+                                path: cwd,
+                                list: list,
+                            };
+                        });
+                    });
+                    */
+                }
+            } else {
+                // Not available
+                updates.active = false;
+                updates.busy = false;
+            }
+        } catch (ex) {
+            updates.busy = false;
+            updates.error = ex;
+        }
+        angular.extend($scope.sqlCmd, updates);
 
     }]);
