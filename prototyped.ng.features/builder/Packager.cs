7c34b6cdf776564aaa1d1e877ebccedd82b8f466
JohnnyBeProgramming <Johnathan.Lodder@Gmail.com>
2015-04-02 19:04:26 +0200

    Added embeders for web resources

---
+++ prototyped.ng.features/builder/Packager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace prototyped.ng.features
+{
+    public class Packager : prototyped.ng.Packager
+    {
+        public override Assembly TargetAssembly
+        {
+            get { return this.GetType().Assembly; }
+        }
+
+        public override void ProcessRequest(HttpContext context)
+        {
+            base.ProcessRequest(context);
+        }
+    }
+}
